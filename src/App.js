a9ced81d152f007b8996b7bfced0c6460426fa14
Darren Oh <darren@oh.name>
2018-01-30 23:20:20 -0500

    Added ability for app to remove contact from database.

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
     }))
+    ContactsAPI.remove(contact)
   }
 
   render() {
